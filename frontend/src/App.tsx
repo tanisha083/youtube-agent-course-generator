869ee47332cc9ab392541010c617d30402dd5a7b
Tanisha Singh <tanisha083btece21@igdtuw.ac.in>
2025-02-23 19:10:24 +0530

    remove unused pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import UploadPage from "./pages/UploadPage";
-import GeneratedCoursePage from "./pages/GeneratedCoursePage";
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/upload" element={<UploadPage />} />
-                <Route path="/generated-course" element={<GeneratedCoursePage />} />
+                {/* <Route path="/" element={<HomePage />} /> */}
+                <Route path="/" element={<UploadPage />} />
             </Routes>
         </Router>
     );
